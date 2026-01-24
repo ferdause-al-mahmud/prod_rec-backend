2044dc03db29da723af17f8c938e7a7d67041648
mahmud444t <ferdausemahmud@gmail.com>
2026-01-24 22:22:58 +0600

    added cors for localhost

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const { MongoClient } = require('mongodb');
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
