c95cea9cc2594a322f4865a38af7b3bb23cda68a
Mahin Ahmed  Dipta <123046479+diptomahin@users.noreply.github.com>
2026-01-25 05:23:10 +0000

    added jwt token api

--- index.js
+++ index.js
@@ -46,6 +46,20 @@ async function connectDB() {
     const queryCollection = database.collection("queries");
     const recommendationCollection = database.collection("recommendations");
 
+
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+    
     //-----------------------
     //Query Api's
     //------------------------
