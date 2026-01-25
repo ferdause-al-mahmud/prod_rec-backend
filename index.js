2f11e63c2754f222cf227a98643ca247da7a679f
mahmud444t <ferdausemahmud@gmail.com>
2026-01-26 01:08:43 +0600

    added chatbot with gemini

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -292,6 +292,12 @@ async function connectDB() {
         res.status(500).send({ message: "Server error" });
       }
     });
+
+    //-----------------------
+    // Chatbot Routes
+    //-----------------------
+    const chatbotRoutes = require('./chatbot-routes');
+    app.use('/api/chatbot', chatbotRoutes(queryCollection, recommendationCollection, verifyToken));
 
     console.log('Connected to MongoDB');
   } catch (err) {
